473d775a212ec1bba614891628b4ab7bb490f157
Sergio Miliati <sergio_v.miliati@hotmail.com>
2025-02-15 03:09:56 -0400

    ajustes finais para o deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // const { NODE_ENV } = process.env;
 // const URL = NODE_ENV === "development" ? "http://localhost:3005/api" : "/api";
 
-const URL = "http://localhost:3005/api";
+const URL = "https://deploysm-projectspotify.onrender.com/api";
 
 const respArtists = await axios.get(`${URL}/artists`);
 const respSongs = await axios.get(`${URL}/songs`);
